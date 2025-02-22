adb587b2a7b2d8130cc4a42cd0471d08664d2938
Jack Kelly <Jack@Jacks-MacBook-Pro.local>
2025-02-22 01:53:18 -0500

    Reset dev branch for Blazor Server Migration

--- backend/routes/home.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from fastapi.responses import RedirectResponse
-
-router = APIRouter()
-
-@router.get("/")
-def home():
-    return RedirectResponse(url="/static/index.html")
